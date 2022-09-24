e345c731cc61dd53a5c3b893f601e81bfc50f1e4
Daiki48 <daiki48.engineer@gmail.com>
2022-09-24 20:38:46 +0900

    [add] FirebaseConfig setting

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.scss";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.scss';
+import { app } from './FirebaseConfig';
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
 		<App />
 	</React.StrictMode>
