7259cec5bde775bb8fccabc93eed7c439ff897c2
Daiki48 <daiki48.engineer@gmail.com>
2022-09-26 18:37:52 +0900

    [update] query -> querySnapshot

--- src/components/showItem.ts
+++ src/components/showItem.ts
@@ -5,10 +5,10 @@ import { Item } from './model';
 
 export const showItem = () => {
 	const itemCollection = collection(db, 'bbs');
-	getDoc(itemCollection).then((query) => {
+	getDoc(itemCollection).then((querySnapshot) => {
 		const itemList: Item[] = [];
 		let count: number = 0;
-		query.docs.map((doc, index) => {
+		querySnapshot.docs.map((doc) => {
 			const item: Item = {
 				docId: doc.id,
 				itemText: doc.data().itemText,
